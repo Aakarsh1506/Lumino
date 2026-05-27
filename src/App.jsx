c0ade03d3ed4f854be0a3c42e9b86968fdfa33c5
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-05-27 17:19:24 +0530

    Changed and added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
+import AuthPage from './components/AuthPage'
 
 function App() {
-  return <LandingPage />
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+    </Routes>
+  )
 }
 
 export default App
