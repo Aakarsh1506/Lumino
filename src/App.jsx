b906186b360a0049c567cfd015716df18a97e418
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-05-29 02:15:32 +0530

    Added New Routings

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import AuthPage from './components/AuthPage'
 import Dashboard from './components/Dashboard'
+import MovieDetail from './components/MovieDetail'   
+import EventDetail from "./components/EventDetail";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/movie/:id" element={<MovieDetail />} /> 
+      <Route path="/event/:id" element={<EventDetail />} /> 
     </Routes>
   )
 }
