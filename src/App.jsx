6d91331e4ce7b6082f76b44bfc9b7c01a50b7c2f
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-05-29 16:41:00 +0530

    Final App Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import { Routes, Route } from 'react-router-dom'
+
 import LandingPage from './components/LandingPage'
 import AuthPage from './components/AuthPage'
 import Dashboard from './components/Dashboard'
-import MovieDetail from './components/MovieDetail'   
-import EventDetail from "./components/EventDetail";
+import MovieDetail from './components/MovieDetail'
+import EventDetail from "./components/EventDetail"
+import CategoryPage from "./components/CategoryPage"
+
+import BookTickets from "./components/BookTickets"
+import EventTicketsPage from "./components/EventTickets"
+import PaymentSuccess from "./components/PaymentSuccess";
 
 function App() {
   return (
     <Routes>
+
+      {/* Core Pages */}
       <Route path="/" element={<LandingPage />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/movie/:id" element={<MovieDetail />} /> 
-      <Route path="/event/:id" element={<EventDetail />} /> 
+
+      {/* Details Pages */}
+      <Route path="/movie/:id" element={<MovieDetail />} />
+      <Route path="/event/:id" element={<EventDetail />} />
+      <Route path="/category/:slug" element={<CategoryPage />} />
+
+      {/* Booking Pages */}
+      <Route path="/book-tickets" element={<BookTickets />} />
+      <Route path="/book-event-tickets" element={<EventTicketsPage />} />
+      <Route path="/payment-success" element={<PaymentSuccess />} />
+
     </Routes>
   )
 }
